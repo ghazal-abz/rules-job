03ce428125c42ddb1b176759d411b76388e0ccc4
ghazal <ghazal.abaszade1@gmail.com>
2023-09-18 13:56:08 +0330

    added animation

--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="job-list">
     <p>Ordered by {{ order }}</p>
-    <ul>
+    <transition-group name="list" tag="ul">
       <li v-for="job in orderedJobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
         <div class="salary">
           <p>{{ job.salary }} rupees</p>
         </div>
         <div class="description">
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem omnis voluptatum eius doloremque optio iusto sequi dignissimos. Pariatur earum assumenda dolores possimus quidem quam, reprehenderit aliquid consequuntur amet non facere.</p>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem omnis voluptatum eius doloremque optio iusto
+            sequi dignissimos. Pariatur earum assumenda dolores possimus quidem quam, reprehenderit aliquid consequuntur
+            amet non facere.</p>
         </div>
       </li>
-    </ul>
+    </transition-group>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, computed } from 'vue'
-import Job from '@/types/Job'
-import OrderTerm from '@/types/OrderTerm'
+import { defineComponent, PropType, computed } from "vue";
+import Job from "@/types/Job";
+import OrderTerm from "@/types/OrderTerm";
 
 export default defineComponent({
   props: {
@@ -35,12 +37,12 @@ export default defineComponent({
     const orderedJobs = computed(() => {
       return [...props.jobs].sort((a: Job, b: Job) => {
         return a[props.order] > b[props.order] ? 1 : -1;
-      })
-    })
+      });
+    });
 
-    return { orderedJobs }
-  },
-})
+    return { orderedJobs };
+  }
+});
 </script>
 
 <style scoped>
@@ -48,9 +50,11 @@ export default defineComponent({
   max-width: 960px;
   margin: 40px auto;
 }
+
 .job-list ul {
-  padding: 0
+  padding: 0;
 }
+
 .job-list li {
   list-style-type: none;
   background: white;
@@ -58,19 +62,27 @@ export default defineComponent({
   margin: 16px 0;
   border-radius: 4px;
 }
+
 .job-list h2 {
   margin: 0 0 10px;
   text-transform: capitalize;
 }
+
 .salary {
   display: flex;
 }
+
 .salary img {
   width: 30px;
 }
+
 .salary p {
   color: #17bf66;
   font-weight: bold;
   margin: 10px 4px;
+}
+
+.list-move {
+  transition: all 1s;
 }
 </style>
